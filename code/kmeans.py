a99de64ab903b9366f7aa6daba1f6c16b07c6a6e
mjvakili <mjvakili@nyu.edu>
2020-02-26 12:33:50 +0100

    segmenting the shear data in the kmeans code

--- code/kmeans.py
+++ code/kmeans.py
@@ -1,8 +1,9 @@
 import kmeans_radec
 import fitsio
 import numpy as np
+from fitsio import FITS
 
-def gal_loader(zmin, zmax):
+def gal_preprocess(zmin, zmax):
 
     lens = fitsio.read("lens.fits", columns = ["ra_gal", "dec_gal", "observed_redshift_gal"])
     ra, dec, z = lens["ra_gal"], lens["dec_gal"], lens["observed_redshift_gal"]
@@ -24,12 +25,13 @@ def gal_loader(zmin, zmax):
     
     return gals, randoms
 
-def shear_loader(zmin, zmax):
+def shear_preprocess(zmin, zmax):
 
-    shear = fitsio.read("source_v2.fits", columns = ["ra_gal", "dec_gal", "observed_redshift_gal"])
+    shear = fitsio.read("source_v2.fits", columns = ["ra_gal", "dec_gal", "observed_redshift_gal", "gamma1", "gamma2"])
     ra, dec, z = shear["ra_gal"], shear["dec_gal"], shear["observed_redshift_gal"]
     g1, g2 = shear["gamma1"], shear["gamma2"]
-    mask = (z>zmax)
+    mask = (z>zmax+0.1)
+    z = z[mask]
     ra, dec = ra[mask], dec[mask]
     g1, g2 = g1[mask], g2[mask]
     print("length of the catalog after applying the cut", len(ra))
@@ -41,9 +43,30 @@ def shear_loader(zmin, zmax):
     print("Done with assigning jacknife labels to galaxies")
     
     gals = {"RA": ra, 
-    	    "DEC": dec, 
-	    "gamma1": g1, 
-	    "gamma2": g2,
-	    "JK_LABEL": gal_labels_jk}
+    	"DEC": dec, 
+	"gamma1": g1, 
+	"gamma2": g2,
+	"redshift": z} 
     
-    return gals
+    fits = FITS('data/shear_zmax_'+str(zmax)+'.fits', 'rw')
+    fits.write(gals, names = ["RA", "DEC", "gamma1", "gamma2", "redshift"])
+    fits.close()
+
+    for jk in range(len(centers)):
+    	
+        gal_jk_mask = gals["JK_LABEL"] != jk
+	gal_jk = {"RA": ra[gal_jk_mask], 
+	    "DEC": dec[gal_jk_mask], 
+	    "gamma1": g1[gal_jk_mask], 
+	    "gamma2": g2[gal_jk_mask],
+	    "redshift": z[gal_jk_mask]}
+        
+	fits = FITS('data/shear_zmax_'+str(zmax)+'_jk_'+str(jk)+'.fits','rw')
+	fits.write(gal_jk, names = ["RA", "DEC", "gamma1", "gamma2", "redshift"])
+        fits.close()
+	
+    return None
+
+if __name__  == '__main__':
+    
+    shear_loader(1.6, 1.7)

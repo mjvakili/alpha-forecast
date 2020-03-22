f5e0dee8efc4d6249413be6941b81e36322fc9f6
mjvakili <mjvakili@nyu.edu>
2020-03-22 16:28:19 +0100

    added noise contributions as a function of separation

--- code/viz_shear.py
+++ code/viz_shear.py
@@ -3,7 +3,6 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
-plt.switch_backend("Agg")
 
 
 class shear_summarizer():
@@ -43,11 +42,11 @@ class shear_summarizer():
         ax.set_ylim([1e-6, .003])
         ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
         plt.tick_params(
-          axis='x',          # changes apply to the x-axis
-          which='minor',      # both major and minor ticks are affected
-          bottom=False,      # ticks along the bottom edge are off
-          top=False,         # ticks along the top edge are off
-          labelbottom=False)    
+          axis='x',         
+          which='minor',               
+	  bottom=False,     
+          top=False,                   
+	  labelbottom=False)
         plt.setp(ax, xticks=[1, 10, 100], xticklabels=['1', '10', '100'])
         ax.set_xlabel(r'$\theta$ [arcmin]', fontsize = 20)
         ax.set_ylabel(r'$\gamma_{t}(\theta)$', fontsize = 20)
@@ -58,8 +57,99 @@ class shear_summarizer():
         plt.title(title)	
 	plt.legend(fontsize = 10, frameon = False, shadow = False)
 	plt.tight_layout()
-        plt.savefig('/home/vakili/public_html/lrg_paper2/shear_completeness_'+str(self.incomp)+'.png', dpi = 600)
+	fname = os.path.join(graph_dir, 'shear_completeness_'+str(self.incomp)+'.png')
+        plt.savefig(fname, dpi = 600)
+        plt.close()
+
+	return None
+    
+    def plot_noise(self, z_pivot):
         
+        if z_pivot in self.z_list:
+	    print("dissecting the noise contributions at pivot redshift.{0:.1f}".format(z_pivot))
+            fig, ax = plt.subplots()
+            pivot_mask = self.z_list == z_pivot
+            shear_clctn = self.shear_clctn[pivot_mask]
+	    theta = shear_clctn["theta"]
+	    jk_noise = np.diag(shear_clctn["jk_cov"])**.5
+	    shot_noise = np.diag(shear_clctn["shot_cov"])**.5
+	    total_noise = np.diag(shear_clctn["total_cov"])**.5
+	    if self.incomp == True:
+	        title = "Pessimistic error bars at {0:.1f} < {1:s} < {2:.1f}".format(z_pivot, r"$z_{\rm H\alpha}$", z_pivot+.1)
+                color = "C3"
+            elif self.incomp == False:
+	        title = "Idealistic error bars at {0:.1f} < {1:s} < {2:.1f}".format(z_pivot, r"$z_{\rm H\alpha}$", z_pivot+.1)
+                color = "C0" 	
+	    ax.plot(theta, total_noise, linewidth = 3, 
+		        linestyle = "solid", color = color, 
+			label = "sqrt of the diagonal elements of the total covariance")
+	    ax.plot(theta, jk_noise, linewidth = 3, 
+		        linestyle = "dashed", color = color, 
+			label = "sqrt of the diagonal elements of the jacknife covariance")
+	    ax.plot(theta, shot_noise, linewidth = 3, 
+		        linestyle = "dashdot", color = color, 
+			label = "shot-noise")
+            ax.set_xscale("log")
+            ax.set_yscale("log")
+            ax.set_xlim([0.9, 120])
+            ax.set_ylim([5e-7, 1e-4])
+            ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+            plt.tick_params(
+                    axis='x',          
+                    which='minor',      
+		    bottom=False,     
+                    top=False,         
+		    labelbottom=False)    
+            plt.setp(ax, xticks=[1, 10, 100], xticklabels=['1', '10', '100'])
+            ax.set_xlabel(r'$\theta$ [arcmin]', fontsize = 20)
+            ax.set_ylabel(r'$\sigma [\gamma_{t}](\theta)$', fontsize = 20)
+            plt.title(title)	
+	    plt.legend(fontsize = 10, frameon = False, shadow = False)
+	    plt.tight_layout()
+	    fname = os.path.join(graph_dir, 'noise_completeness_'+str(self.incomp)+'.png')
+            plt.savefig(fname, dpi = 600)
+            plt.close()
+	else:
+	    print("the pivot redshift needs to be in the provided list")
+	    raise ValueError
+
+
+    def plot_SNR(self):
+        '''
+	shows the redshift evolution of 
+	the signal-to-noise and different 
+	contributions to it
+	'''
+	total_snr, jk_snr, shot_snr = [], [], []
+        fig, ax = plt.subplots()
+        for z_indx, z in enumerate(self.z_list):
+
+	    shear_dict = self.shear_clctn[z_indx]
+            total_snr.append(shear_dict["total_snr"])
+            jk_snr.append(shear_dict["jk_snr"])
+            shot_snr.append(shear_dict["shot_snr"])
+        
+	if self.incomp == True:
+	    title = "Pessimistic lensing signal-to-noise ratio"
+            color = "C3"
+        elif self.incomp == False:
+	    title = "Idealistic lensing signal-to-noise ratio"
+            color = "C0" 	
+	ax.plot(np.array(self.z_list) +.05, total_snr, linewidth = 3, linestyle = "solid", color = color, label = "signal-to-noise ratio")
+	ax.plot(np.array(self.z_list) +.05, jk_snr, linewidth = 3, linestyle = "dashed", color = color, label = "signal-to-noise ratio jacknife only")
+	ax.plot(np.array(self.z_list) +.05, shot_snr, linewidth = 3, linestyle = "dashdot", color = color, label = "signal-to-noise ratio shot-noise only")
+        ax.set_xlim([0.85, 1.65])
+        ax.set_ylim([8, 170])
+        ax.set_yscale('log')
+	ax.set_xlabel(r'$z_{\rm H\alpha}$', fontsize = 20)
+        ax.set_ylabel(r'S/N $[\gamma_{t}]$', fontsize = 20)
+        plt.title(title)	
+	plt.legend(fontsize = 10, frameon = False, shadow = False)
+	plt.tight_layout()
+        fname = os.path.join(graph_dir, 'snr_completeness_'+str(self.incomp)+'.png')
+	plt.savefig(fname, dpi = 600)
+        plt.close()
+
 	return None
 
 def shear_extractor(zmin = 1.0, incomp = True, shape_noise = 0.3):
@@ -84,11 +174,11 @@ def shear_extractor(zmin = 1.0, incomp = True, shape_noise = 0.3):
     #total covariance
     total_cov = jk_cov + shot_cov
     #total signal-to-noise ratio (snr)
-    total_snr = np.dot(xi, np.linalg.solve(total_cov, xi))
+    total_snr = np.dot(xi.T, np.linalg.solve(total_cov, xi))**0.5
     # jacknife snr
-    jk_snr = np.dot(xi, np.linalg.solve(jk_cov, xi))
+    jk_snr = np.dot(xi.T, np.linalg.solve(jk_cov, xi))**0.5
     # Poisson snr
-    shot_snr = np.dot(xi, np.linalg.solve(shot_cov, xi))
+    shot_snr = np.dot(xi.T, np.linalg.solve(shot_cov, xi))**0.5
     # every relevant info in a dictionary placeholder 
     shear_dict = {"theta": theta, 
                   "xi" : xi, 
@@ -105,6 +195,7 @@ def matplotlib_settings():
     '''
     configuration of matplotlib settings
     '''
+    plt.switch_backend("Agg")
     matplotlib.rcParams['xtick.major.size'] = 7
     matplotlib.rcParams['xtick.labelsize'] = 'x-large'
     matplotlib.rcParams['ytick.major.size'] = 7
@@ -122,16 +213,21 @@ def label_decoder(zmin):
     """figure friendly labels 
     for the redshift bins
     """
-    zmax, zs = zmin + 0.1, zmin + 0.1
-    label = r"${0:.1f}<z<{1:.1f}, \; z>{2:.1f}$".format(zmin, zmax, zs)
+    zmax, zs = zmin + 0.1, zmin + 0.2
+    label = "{0:.1f}< {3:s} <{1:.1f},  {4:s}>{2:.1f}".format(zmin, zmax, zs, r"$z_{\rm H\alpha}$", r"$z_{\rm s}$")
 
     return label
 
 if __name__ == '__main__':
     
+
     matplotlib_settings()
-    z_list = [0.9, 1.1, 1.2, 1.3, 1.4, 1.5]
-    incomp = False
+    z_list = [0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5]
     shape_noise = 0.3
-    summarizer = shear_summarizer(z_list, incomp, shape_noise)
-    summarizer.plot_shear()
+    graph_dir = "/home/vakili/public_html/lrg_paper2"
+    for incomp in [False, True]:
+        
+	summarizer = shear_summarizer(z_list, incomp, shape_noise)
+        summarizer.plot_shear()
+        summarizer.plot_SNR()
+	summarizer.plot_noise(1.0)
